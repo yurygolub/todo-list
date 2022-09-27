26d9a19e620d5e89c35a8ed5fd19eb319331d0a1
Yury Golub <yuragolub21@gmail.com>
2022-09-27 22:22:42 +0300

    Remove useless files. Some fixes.

---
+++ ToDoApp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var cultureInfo = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), 
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ ToDoApp/models/TodoModel.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+
+namespace ToDoApp.models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+
+        private bool _isDone;
+        private string _text;
+
+        [JsonProperty(PropertyName = "creationDate")]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        [JsonProperty(PropertyName = "isDone")]
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            { 
+                if(_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
